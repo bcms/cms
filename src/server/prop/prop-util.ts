f669999d2955b937668819d21b7ee1814ba875ad
Bbanez <banezprod@gmail.com>
2020-05-19 11:43:11 +0200

    Fix problem with buffer and objects

--- src/server/prop/prop-util.ts
+++ src/server/prop/prop-util.ts
@@ -917,7 +917,10 @@ export class PropUtil {
         content.push({
           type: prop.type,
           name,
-          value: Buffer.from(value, 'latin1').toString('utf8'),
+          value:
+            typeof value === 'string'
+              ? Buffer.from(value, 'latin1').toString('utf8')
+              : value,
         });
       }
     }
