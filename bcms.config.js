cde06ab0b3847fe96053135f51405290f3860bb9
Bbanez <banezprod@gmail.com>
2022-02-17 15:48:09 +0100

    Set default port to 8080

--- bcms.config.js
+++ bcms.config.js
@@ -1,7 +1,7 @@
 const { createBcmsConfig } = require('@becomes/cms-backend/config');
 
 module.exports = createBcmsConfig({
-  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 1280,
+  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 8080,
   local: true,
   jwt: {
     expireIn: process.env.JWT_EXP_AFTER
