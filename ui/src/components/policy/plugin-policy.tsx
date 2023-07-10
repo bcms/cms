89e6df7b8fa213978ff40db9144c8f6e1c0f5c67
boris-becomes <boris.bosnjak@becomes.co>
2023-07-10 12:28:37 +0200

    fix: permission ui

--- ui/src/components/policy/plugin-policy.tsx
+++ ui/src/components/policy/plugin-policy.tsx
@@ -5,7 +5,8 @@ import type {
 } from '@becomes/cms-sdk/types';
 import { computed, defineComponent, type PropType } from 'vue';
 import { useTranslation } from '../../translations';
-import { BCMSCheckboxInput, BCMSSelect } from '../input';
+import { BCMSCheckboxInput, BCMSSelect, BCMSTextInput } from '../input';
+import { BCMSIcon } from '..';
 
 const component = defineComponent({
   props: {
@@ -35,7 +36,7 @@ const component = defineComponent({
     });
     function handleOptionChange(
       schema: BCMSPluginPolicy,
-      options: BCMSUserPolicyPluginOption
+      options: BCMSUserPolicyPluginOption,
     ) {
       const optionsMap: {
         [name: string]: BCMSUserPolicyPluginOption;
@@ -68,7 +69,7 @@ const component = defineComponent({
       }
       ctx.emit(
         'changeOption',
-        Object.keys(optionsMap).map((e) => optionsMap[e])
+        Object.keys(optionsMap).map((e) => optionsMap[e]),
       );
     }
 
@@ -89,12 +90,12 @@ const component = defineComponent({
         />
         {props.policySchema.length > 0 && !props.policy.fullAccess ? (
           <div class="pluginPolicy--options">
-            <h4 class="font-semibold">
+            <h4 class="text-lg mb-4 dark:text-light">
               {translations.value.page.plugin.title}
             </h4>
             {props.policySchema.map((schema) => {
               const policyOption = props.policy.options.find(
-                (e) => e.name === schema.name
+                (e) => e.name === schema.name,
               );
               if (schema.type === 'checkbox') {
                 return (
@@ -130,15 +131,128 @@ const component = defineComponent({
                     }
                     selected={policyOption ? policyOption.value[0] : ''}
                     onChange={(option) => {
-                      if (option.value) {
-                        ctx.emit('changeAccess', true);
-                      }
                       handleOptionChange(schema, {
                         name: schema.name,
                         value: [option.value],
                       });
                     }}
+                    class="mb-5"
                   />
+                );
+              } else if (schema.type === 'selectArray') {
+                return (
+                  <div>
+                    <div class="font-normal not-italic text-xs leading-normal tracking-0.06 select-none uppercase block mb-1.5  dark:text-light">
+                      {window.bcms.util.string.toPretty(schema.name)}
+                    </div>
+                    <div class="grid grid-cols-1 gap-3 mb-5">
+                      {((policyOption?.value || []).length > 0 ? policyOption?.value : ['false'])?.map((_, i) => {
+                        return (
+                          <div key={i} class="flex items-end gap-4">
+                            <BCMSSelect
+                              placeholder={window.bcms.util.string.toPretty(
+                                schema.name,
+                              )}
+                              options={
+                                schema.options
+                                  ? schema.options.map((e) => {
+                                      return {
+                                        label: e,
+                                        value: e,
+                                      };
+                                    })
+                                  : []
+                              }
+                              selected={
+                                policyOption ? policyOption.value[i] : ''
+                              }
+                              onChange={(option) => {
+                                const value = policyOption?.value || []
+                                value[i] = option.value
+                                handleOptionChange(schema, {
+                                  name: schema.name,
+                                  value,
+                                });
+                              }}
+                              class="flex-1"
+                            />
+                            {i === (policyOption?.value || [])?.length - 1 && (
+                              <button
+                                class="flex group"
+                                onClick={() => {
+                                  policyOption?.value.push('');
+                                }}
+                              >
+                                <BCMSIcon
+                                  src="/plus-circle"
+                                  class="text-dark fill-current w-6 h-6 mb-3 transition-colors duration-200 group-hover:text-green group-focus-visible:text-green dark:text-light dark:group-hover:text-yellow dark:group-focus-visible:text-yellow"
+                                />
+                              </button>
+                            )}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              } else if (schema.type === 'input') {
+                return (
+                  <BCMSTextInput
+                    label={window.bcms.util.string.toPretty(schema.name)}
+                    placeholder={window.bcms.util.string.toPretty(schema.name)}
+                    value={policyOption ? policyOption.value[0] : ''}
+                    onInput={(value) => {
+                      handleOptionChange(schema, {
+                        name: schema.name,
+                        value: [value],
+                      });
+                    }}
+                    class="mb-5"
+                  />
+                );
+              } else if (schema.type === 'inputArray') {
+                return (
+                  <div>
+                    <div class="font-normal not-italic text-xs leading-normal tracking-0.06 select-none uppercase block mb-1.5  dark:text-light">
+                      {window.bcms.util.string.toPretty(schema.name)}
+                    </div>
+                    <div class="grid grid-cols-1 gap-3 mb-5">
+                      {policyOption?.value?.map((_, i) => {
+                        return (
+                          <div key={i} class="flex items-end gap-4">
+                            <BCMSTextInput
+                              placeholder={window.bcms.util.string.toPretty(
+                                schema.name,
+                              )}
+                              value={policyOption ? policyOption.value[i] : ''}
+                              onInput={(value) => {
+                                const val = policyOption?.value || []
+                                val[i] = value
+                                handleOptionChange(schema, {
+                                  name: schema.name,
+                                  value: val,
+                                });
+                              }}
+                              class="flex-1"
+                            />
+                            {i === (policyOption?.value || [])?.length - 1 && (
+                              <button
+                                class="flex group"
+                                onClick={() => {
+                                  policyOption?.value?.push('false');
+                                }}
+                              >
+                                <BCMSIcon
+                                  src="/plus-circle"
+                                  class="text-dark fill-current w-6 h-6 mb-3 transition-colors duration-200 group-hover:text-green group-focus-visible:text-green dark:text-light dark:group-hover:text-yellow dark:group-focus-visible:text-yellow"
+                                />
+                              </button>
+                            )}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
                 );
               } else {
                 return <div style="display: none;" />;
