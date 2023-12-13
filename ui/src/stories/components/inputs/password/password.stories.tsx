ca9cfbadf132772e31a019dbc8590cd368a5bf10
boris-becomes <boris.bosnjak@becomes.co>
2023-12-13 12:56:54 +0100

    Update Password story

--- ui/src/stories/components/inputs/password/password.stories.tsx
+++ ui/src/stories/components/inputs/password/password.stories.tsx
@@ -6,54 +6,61 @@ import BCMSPasswordInput from './Index.tsx';
 const meta: Meta<typeof BCMSPasswordInput> = {
   title: 'Components/Inputs/Password',
   component: BCMSPasswordInput,
-  // tags: ['autodocs'],
   render: (args) => ({
     components: {
       BCMSPasswordInputPreview,
     },
     setup() {
       return {
-        args,
+        props: args,
       };
     },
     template: `
       <BCMSPasswordInputPreview
-        :class="args.class"
-        :value="args.value"
-        :placeholder="args.placeholder"
-        :label="args.label"
-        :invalid-text="args.invalidText"
-        :disabled="args.disabled"
-        :helper-text="args.helperText"
-        :readonly="args.readonly"
-        @input="($event) => args.value = $event.value"
+        :class="props.class"
+        :value="props.value"
+        :placeholder="props.placeholder"
+        :label="props.label"
+        :invalid-text="props.invalidText"
+        :disabled="props.disabled"
+        :helper-text="props.helperText"
+        :readonly="props.readonly"
+        @input="($event) => props.value = $event.value"
       />
     `,
   }),
   argTypes: {
     value: {
-      control: 'number',
+      control: 'text',
+      description: 'Text',
     },
     class: {
       control: 'text',
+      description: 'TailwindCSS or custom CSS classes',
     },
     placeholder: {
       control: 'text',
+      description: 'Placeholder text',
     },
     label: {
       control: 'text',
+      description: 'Password label',
     },
     invalidText: {
       control: 'text',
+      description: 'Error text',
     },
     disabled: {
       control: 'boolean',
+      description: 'Disable password interaction',
     },
     helperText: {
       control: 'text',
+      description: 'Helper text',
     },
     readonly: {
       control: 'boolean',
+      description: 'Prevent text change',
     },
   },
 };
@@ -61,7 +68,7 @@ const meta: Meta<typeof BCMSPasswordInput> = {
 export default meta;
 type Story = StoryObj<typeof BCMSPasswordInput>;
 
-export const Playground: Story = {
+export const Preview: Story = {
   parameters: {
     docs: {
       source: {
@@ -102,7 +109,6 @@ export const Playground: Story = {
 export const Disabled: Story = {
   args: {
     label: 'Disabled',
-    disabled: true,
   },
   argTypes: {
     disabled: {
@@ -115,20 +121,20 @@ export const Disabled: Story = {
     },
     setup() {
       return {
-        args,
+        props: args,
       };
     },
     template: `
     <BCMSPasswordInput
-      :class="args.class"
-      :value="args.value"
-      :placeholder="args.placeholder"
-      :label="args.label"
-      :invalid-text="args.invalidText"
-      :disabled="args.disabled"
-      :helper-text="args.helperText"
-      :readonly="args.readonly"
-      @input="args.value = $event"
+      :class="props.class"
+      :value="props.value"
+      :placeholder="props.placeholder"
+      :label="props.label"
+      :invalid-text="props.invalidText"
+      disabled
+      :helper-text="props.helperText"
+      :readonly="props.readonly"
+      @input="props.value = $event"
     />
     `,
   }),
@@ -149,20 +155,20 @@ export const Error: Story = {
     },
     setup() {
       return {
-        args,
+        props: args,
       };
     },
     template: `
     <BCMSPasswordInput
-      :class="args.class"
-      :value="args.value"
-      :placeholder="args.placeholder"
-      :label="args.label"
-      :invalid-text="args.invalidText"
-      :disabled="args.disabled"
-      :helper-text="args.helperText"
-      :readonly="args.readonly"
-      @input="args.value = $event"
+      :class="props.class"
+      :value="props.value"
+      :placeholder="props.placeholder"
+      :label="props.label"
+      :invalid-text="props.invalidText"
+      :disabled="props.disabled"
+      :helper-text="props.helperText"
+      :readonly="props.readonly"
+      @input="props.value = $event"
     />
     `,
   }),
