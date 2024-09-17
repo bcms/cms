9a362f06e738502a074c76e72fc906f5ff62bc9a
bbanez <banezprod@gmail.com>
2024-09-17 16:47:19 +0200

    standalone: add sdk build pipeline

--- _scripts/sdk.ts
+++ _scripts/sdk.ts
@@ -15,36 +15,62 @@ export async function packSdk() {
 }
 
 export async function buildSdk() {
-    const basePath = path.join(process.cwd(), 'client');
+    const basePath = path.join(process.cwd(), 'ui');
     const localFs = new FS(basePath);
-    if (await localFs.exist(['dist'])) {
-        await localFs.deleteDir(['dist']);
+    if (await localFs.exist(['dist-sdk'])) {
+        await localFs.deleteDir(['dist-sdk']);
     }
-    await buildMjs(localFs, basePath, 'build:ts:mjs', 'dist');
-    await buildCjs(localFs, basePath, 'build:ts:cjs', 'dist');
-    const fileNames = await localFs.readdir(['dist', 'client', 'src']);
-    for (let i = 0; i < fileNames.length; i++) {
-        const fileName = fileNames[i];
-        await localFs.move(
-            ['dist', 'client', 'src', fileName],
-            ['dist', fileName],
+    await buildMjs(localFs, basePath, 'build:sdk:mjs', 'dist-sdk', async () => {
+        const delDirs = ['backend'];
+        for (let i = 0; i < delDirs.length; i++) {
+            const delDir = delDirs[i];
+            await localFs.deleteDir(['dist-sdk', 'mjs', delDir]);
+        }
+        const files = await localFs.fileTree(
+            ['dist-sdk', 'mjs', 'ui', 'src', 'sdk'],
+            '',
         );
-    }
-    await localFs.deleteDir(['dist', 'backend']);
-    await localFs.deleteDir(['dist', 'client']);
-    await localFs.copy('README.md', ['dist', 'README.md']);
-    await localFs.copy('LICENSE', ['dist', 'LICENSE']);
-    const packageJson = JSON.parse(await localFs.readString('package.json'));
+        for (let i = 0; i < files.length; i++) {
+            const fileInfo = files[i];
+            await localFs.move(
+                ['dist-sdk', 'mjs', 'ui', 'src', 'sdk', fileInfo.path.rel],
+                ['dist-sdk', 'mjs', fileInfo.path.rel],
+            );
+        }
+    });
+    await buildCjs(localFs, basePath, 'build:sdk:cjs', 'dist-sdk', async () => {
+        const delDirs = ['backend'];
+        for (let i = 0; i < delDirs.length; i++) {
+            const delDir = delDirs[i];
+            await localFs.deleteDir(['dist-sdk', 'cjs', delDir]);
+        }
+        const files = await localFs.fileTree(
+            ['dist-sdk', 'cjs', 'ui', 'src', 'sdk'],
+            '',
+        );
+        for (let i = 0; i < files.length; i++) {
+            const fileInfo = files[i];
+            await localFs.move(
+                ['dist-sdk', 'cjs', 'ui', 'src', 'sdk', fileInfo.path.rel],
+                ['dist-sdk', 'cjs', fileInfo.path.rel],
+            );
+        }
+    });
+    await localFs.copy('SDK-README.md', ['dist-sdk', 'README.md']);
+    await localFs.copy(['..', 'LICENSE'], ['dist-sdk', 'LICENSE']);
+    const packageJson = JSON.parse(
+        await localFs.readString('sdk.package.json'),
+    );
     packageJson.devDependencies = undefined;
     packageJson.scripts = {};
     const backendVersion = await getBackendVersion();
     const utilsVersion = await getUtilsVersion();
     packageJson.dependencies[backendVersion[0]] = '^' + backendVersion[1];
     packageJson.dependencies[utilsVersion[0]] = '^' + utilsVersion[1];
-    let files = await localFs.fileTree(['dist'], '');
+    let files = await localFs.fileTree(['dist-sdk'], '');
     packageJsonExport(files, packageJson);
     await localFs.save(
-        ['dist', 'package.json'],
+        ['dist-sdk', 'package.json'],
         JSON.stringify(packageJson, null, 4),
     );
 }
